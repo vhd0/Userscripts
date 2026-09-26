9acbec58c76c0d3c39acd554d5d3791768663d95
vhd0 <vhd0@users.noreply.github.com>
2026-09-26 12:05:45 +0000

    release

---
+++ release/Smoothscroll.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Smoothscroll
+// @author       Creec Winceptor
+// @namespace https://greasyfork.org/users/3167
+// @include     *
+// @version 12
+// @downloadURL https://raw.githubusercontent.com/vhd0/Userscripts/release/release/Smoothscroll.user.js
+// @updateURL https://raw.githubusercontent.com/vhd0/Userscripts/release/release/Smoothscroll.meta.js
+// ==/UserScript==
